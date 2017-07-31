0c4ca357543b18ef12c36bcb326baac22f8f02ce
getElementByName <korywh@gmail.com>
2017-08-01 07:36:17 +0900

    feat: base features

--- src/StateStore.ts
+++ src/StateStore.ts
@@ -1,5 +1,55 @@
-class StateStore extends eg.Component {
+interface ChangeHandlerInterface{
+    (newState: any, preState: any): void;
+}
+
+interface MiddlewareInterface<StateType>{
+    (next: (state:StateType)=> StateType): any;
+}
+
+
+class StateStore<StateType>{
+    handlerList: ChangeHandlerInterface[];
+
+    state: StateType;
+
+    constructor(initState: StateType) {
+        this.handlerList = [];
+        this.state = initState;
+    }
+
+    onChange(handler: ChangeHandlerInterface, selector?: Function){
+        let warppedHandler: ChangeHandlerInterface;
+        if(selector){
+            warppedHandler = (newState: any, preState: any)=>{
+                const newTarget = selector(newState);
+                const preTarget = selector(preState);
+                if(newTarget !== preTarget){
+                    handler(newTarget, preTarget);
+                }
+            }
+        } else {
+            warppedHandler = handler;
+        }
+
+        this.handlerList.push(warppedHandler);
+    }
+
+    // TODO: mixin
+
     
+    setState(state: StateType): any{
+        const preState = this.state;
+        const newState = state;
+        this.state = newState;
+
+        // TODO: middleware
+
+        const handlerList = this.handlerList;
+        for(let i=0; i<handlerList.length; i++){
+            const nowHandler = handlerList[i];
+            nowHandler(newState, preState);
+        }
+    }
 }
 
 export {StateStore};
--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,27 @@
 import {StateStore} from "./index";
 
-const stateStore = new StateStore();
+const stateStore = new StateStore<any>({a:1});
 
-console.log(stateStore);
+
+
+
+stateStore.onChange((newState)=>{
+    console.log(newState);
+}, (state: any)=>{
+    return state.a;
+});
+
+stateStore.setState({
+    a:1,
+    b:2
+});
+
+stateStore.setState({
+    a:1,
+    b:3
+});
+
+stateStore.setState({
+    a:2,
+    b:3
+});
